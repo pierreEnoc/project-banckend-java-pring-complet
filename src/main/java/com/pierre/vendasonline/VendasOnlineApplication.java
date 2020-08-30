4df52d8bb1f3fc593d0e78d9eacc896ee6580b96
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-30 19:47:59 -0300

    :sparkles: create operation the instantiation

--- src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
+++ src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
@@ -1,13 +1,35 @@
 package com.pierre.vendasonline;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pierre.vendasonline.domain.Categoria;
+import com.pierre.vendasonline.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class VendasOnlineApplication {
+public class VendasOnlineApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasOnlineApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 
 }
