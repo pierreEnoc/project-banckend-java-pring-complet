1d04f63eb2af4c32d9b4490f8dfea7f0cc530942
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-30 18:35:47 -0300

    :sparkles: database h2, add entity and categoria

--- src/main/java/com/pierre/vendasonline/domain/Categoria.java
+++ src/main/java/com/pierre/vendasonline/domain/Categoria.java
@@ -2,10 +2,15 @@ package com.pierre.vendasonline.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private String nome;
 	
