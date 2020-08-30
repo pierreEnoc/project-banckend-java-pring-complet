287cfa4629d6099c0282f33149c6b0e59cdf9990
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-30 17:21:47 -0300

    :sparkles: start project

---
+++ src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.pierre.vendasonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pierre/vendasonline/VendasOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierre.vendasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
