a8e73e93f82d40b66f56b5cdf3fe2e89d6551427
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-30 20:47:44 -0300

    :sparkles: produto association n * n with category

---
+++ src/main/java/com/pierre/vendasonline/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.pierre.vendasonline.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pierre.vendasonline.domain.Produto;
+
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
