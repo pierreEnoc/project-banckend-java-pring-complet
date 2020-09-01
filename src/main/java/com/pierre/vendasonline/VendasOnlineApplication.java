d54e2468376c9f9280942a49f5592af53fbb64d3
pierreEnoc <pierre_enoc@yahoo.fr>
2020-08-31 22:27:34 -0300

    :sparkles: add state and city

--- src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
+++ src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pierre.vendasonline.domain.Categoria;
+import com.pierre.vendasonline.domain.Cidade;
+import com.pierre.vendasonline.domain.Estado;
 import com.pierre.vendasonline.domain.Produto;
 import com.pierre.vendasonline.repositories.CategoriaRepository;
+import com.pierre.vendasonline.repositories.CidadeRepository;
+import com.pierre.vendasonline.repositories.EstadoRepository;
 import com.pierre.vendasonline.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class VendasOnlineApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 
 	public static void main(String[] args) {
@@ -43,9 +53,22 @@ public class VendasOnlineApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 =  new Estado(null,"Minas Gerais");
+		Estado est2 =  new Estado(null,"São paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Sao paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
