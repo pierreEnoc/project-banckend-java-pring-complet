af824dcf42192f9623579139fa43e8f5647ab328
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-03 16:29:58 -0300

    :recycle: update corrections in service categories and payment constructor

--- src/main/java/com/pierre/vendasonline/domain/Pagamento.java
+++ src/main/java/com/pierre/vendasonline/domain/Pagamento.java
@@ -10,7 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pierre.vendasonline.domain.enums.EstadoPagamento;
 
@@ -37,7 +36,7 @@ private static final long serialVersionUID = 1L;
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado.getCod();
+		this.estado = (estado == null) ? null : estado.getCod();
 		this.pedido = pedido;
 	}
 
