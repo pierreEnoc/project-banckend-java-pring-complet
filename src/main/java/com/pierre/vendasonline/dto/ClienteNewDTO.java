f2a240367473b84529e243d4d091991277405e4f
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-11 16:39:25 -0300

    :sparkles: add password to client

--- src/main/java/com/pierre/vendasonline/dto/ClienteNewDTO.java
+++ src/main/java/com/pierre/vendasonline/dto/ClienteNewDTO.java
@@ -26,6 +26,9 @@ public class ClienteNewDTO implements Serializable {
 	private String cpfOuCnpj;
 
 	private Integer tipo;
+	
+	@NotEmpty(message = "Preechimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message = "Preechimento obrigatório")
 	private String logradouro;
@@ -155,6 +158,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
