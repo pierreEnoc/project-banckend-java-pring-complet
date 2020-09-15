c9490223141a3347e5a5479b72ab7d63a5ffad5b
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-15 15:34:41 -0300

    :sparkles: customer content restriction only retrieves your orders

--- src/main/java/com/pierre/vendasonline/repositories/PedidoRepository.java
+++ src/main/java/com/pierre/vendasonline/repositories/PedidoRepository.java
@@ -1,9 +1,18 @@
 package com.pierre.vendasonline.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pierre.vendasonline.domain.Cliente;
 import com.pierre.vendasonline.domain.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly= true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/pierre/vendasonline/services/PedidoService.java
+++ src/main/java/com/pierre/vendasonline/services/PedidoService.java
@@ -4,15 +4,22 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.pierre.vendasonline.domain.Cliente;
 import com.pierre.vendasonline.domain.ItemPedido;
 import com.pierre.vendasonline.domain.PagamentoComBoleto;
 import com.pierre.vendasonline.domain.Pedido;
 import com.pierre.vendasonline.domain.enums.EstadoPagamento;
+import com.pierre.vendasonline.repositories.ClienteRepository;
 import com.pierre.vendasonline.repositories.ItemPedidoRepository;
 import com.pierre.vendasonline.repositories.PagamentoRepository;
 import com.pierre.vendasonline.repositories.PedidoRepository;
+import com.pierre.vendasonline.security.UserSS;
+import com.pierre.vendasonline.services.exceptions.AuthorizationException;
 import com.pierre.vendasonline.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -75,4 +82,14 @@ public class PedidoService {
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		 if(user == null) {
+			 throw new AuthorizationException("Acesso negado");
+		 }
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);	
+	}
+
 }
