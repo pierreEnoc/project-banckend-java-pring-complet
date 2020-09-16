6aee8d209f1840b2d26e8cad9d7121912a6a0c6c
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-16 12:19:01 -0300

    :sparkles: save first image s3

--- src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
+++ src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
@@ -1,12 +1,17 @@
 package com.pierre.vendasonline;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pierre.vendasonline.services.S3Service;
+
 @SpringBootApplication
 public class VendasOnlineApplication implements CommandLineRunner {
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasOnlineApplication.class, args);
@@ -15,7 +20,7 @@ public class VendasOnlineApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-
+		s3Service.uploadFile("/home/pierre/Documents/meus-projetos/Backend/java/vendas/imagens/avatar-blank.png");
 	}	
 	
 }
