6e7a9f8dbaa74e002cb0c1d810cbab52e665dd1d
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-19 22:16:05 -0300

    :sparkles: reconfig prod

--- src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
+++ src/main/java/com/pierre/vendasonline/VendasOnlineApplication.java
@@ -1,11 +1,8 @@
 package com.pierre.vendasonline;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.pierre.vendasonline.services.S3Service;
 
 @SpringBootApplication
 public class VendasOnlineApplication implements CommandLineRunner {
