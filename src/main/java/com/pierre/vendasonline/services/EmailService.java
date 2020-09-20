a2d02b0138ffdc3ffe30bd5e9430489ca96e6b4e
pierreEnoc <pierre_enoc@yahoo.fr>
2020-09-19 22:34:33 -0300

    refactor add notation component in email service

--- src/main/java/com/pierre/vendasonline/services/EmailService.java
+++ src/main/java/com/pierre/vendasonline/services/EmailService.java
@@ -3,10 +3,12 @@ package com.pierre.vendasonline.services;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
 import com.pierre.vendasonline.domain.Cliente;
 import com.pierre.vendasonline.domain.Pedido;
 
+@Component
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
